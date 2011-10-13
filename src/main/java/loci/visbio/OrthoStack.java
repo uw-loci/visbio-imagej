14b36cab868c346f709f72630dd938f3f00f58c3
Curtis Rueden <ctrueden@wisc.edu>
2011-10-13 20:54:00 +0000

    Use a finer grained stretch step.

--- src/main/java/loci/visbio/OrthoStack.java
+++ src/main/java/loci/visbio/OrthoStack.java
@@ -86,7 +86,7 @@ public class OrthoStack extends JPanel implements PlugIn, AdjustmentListener,
 	private static final double INITIAL_STRETCH = 50;
 	private static final double MIN_STRETCH = 1;
 	private static final double MAX_STRETCH = 1000;
-	private static final double STEP_STRETCH = 1;
+	private static final double STEP_STRETCH = 0.5;
 
 	private static final double INITIAL_ANGLE = 75;
 	private static final double MIN_ANGLE = 0;
